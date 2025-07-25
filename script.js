10f43df41a87cc218fe043c50c0474646186fce7
Mustapha M. Salisu <taphakooli@gmail.com>
2025-07-25 09:40:57 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 5000;
+
+valueDisplays.forEach((valueDisplays) =>{
+    let startValue = 0;
+    let endValue = parseInt(valueDisplays.getAttribute
+        ("data"));
+        console .log
+});
